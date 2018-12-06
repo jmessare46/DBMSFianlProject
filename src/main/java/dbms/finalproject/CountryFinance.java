cc0f9102aba618dcfeffaa100b4b78497a520ecd
jmessare46 <jmessare46@gmail.com>
2018-12-06 15:34:14 -0500

    Recreated country object for new tables

--- src/main/java/dbms/finalproject/CountryFinance.java
+++ src/main/java/dbms/finalproject/CountryFinance.java
@@ -1,17 +1,81 @@
 package dbms.finalproject;
 
-import java.sql.SQLException;
-import java.util.List;
+public class CountryFinance
+{
+    private String country;
+    private String subjectDescription;
+    private String unit;
+    private String scale;
+    private String notes;
+    private Integer year;
+    private Double value;
+    private Integer uid;
 
-public interface CountryFinance {
-    CountryObj getAttacker(String country, String subject, Integer year) throws SQLException;
 
-    List<CountryObj> getAll() throws SQLException;
+    public CountryFinance(String country, String subjectDescription, String unit, String scale, String notes, Integer year, Double value, Integer uid) {
+        this.country = country;
+        this.subjectDescription = subjectDescription;
+        this.unit = unit;
+        this.scale = scale;
+        this.notes = notes;
+        this.year = year;
+        this.value = value;
+        this.uid = uid;
+    }
 
-    void save(CountryObj attacker) throws SQLException;
+    public String getCountry() {
+        return country;
+    }
 
-    void update(CountryObj attacker, String[] params) throws SQLException;
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-    void delete(CountryObj attacker) throws SQLException;
+    public String getSubjectDescription() {
+        return subjectDescription;
+    }
 
+    public void setSubjectDescription(String subjectDescription) {
+        this.subjectDescription = subjectDescription;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getScale() {
+        return scale;
+    }
+
+    public void setScale(String scale) {
+        this.scale = scale;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
